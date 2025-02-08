f422b2f04b55ddd8a35f86c98a37ae9d9ac661c4
Rahul Sajeevan <144695000+PaiGoManh@users.noreply.github.com>
2025-02-08 09:35:58 +0530

    Update certificate.sol

--- Certificate_Sample/certificate.sol
+++ Certificate_Sample/certificate.sol
@@ -20,7 +20,7 @@ contract Cert{
         _;
     }
 
-    mapping (uint=>certificate) public certificates;
+    mapping (uint => certificate) public certificates;
 
     function issue(uint _id,string memory _name,string memory _course,string memory _grade,string memory _date) public onlyAdmin{   
         certificates[_id] = certificate(_name,_course,_grade,_date);
